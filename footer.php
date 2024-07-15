8b2deb265d7f958392b7c34e445d6e95da07a69c
github <github>
2024-07-16 00:31:09 +0900

    デザイン修正

--- footer.php
+++ footer.php
@@ -8,11 +8,11 @@
       <section class="item">
         <h3 class="footer-title">About</h3>
         <p>
-        歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者のオリジナルテーマを使っています。
+        歴史が好きな有閑マダムを目指す普通の会社員です。<br>こちらのブログは作者のオリジナルテーマを使っています。
         </p>
       </section>
     </div>
-    <div class="copyright">© devsugar<a href="https://github.com/dev-milk/sugar-design-wp">&nbsp;original theme</a></div>
+    <div class="copyright">© sugar.geo.jp</a></div>
     <a href="<?php echo esc_url(home_url('#top')); ?>" class="gotop">TOP</a>
   </footer></body>
 </html>
