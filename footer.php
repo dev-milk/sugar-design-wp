14834a4652530a09312f1a786cb486f55884cb2b
github <github>
2024-07-15 18:52:36 +0900

    footer修正

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer id="footer">
     <div class="content wrapper">
       <section class="item">
-        <a href="index.html">
+        <a href="index.php">
           <img class="footer-logo" src="<?php echo get_theme_file_uri( 'img/site-logo.svg' ); ?>" alt="サイトロゴ" />
         </a>      
       </section>
       <section class="item">
         <h3 class="footer-title">About</h3>
         <p>
-        このブログは作者のオリジナルテーマを使っています。
+        歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者のオリジナルテーマを使っています。
         </p>
       </section>
     </div>
