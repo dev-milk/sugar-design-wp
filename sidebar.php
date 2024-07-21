574a55e7f887e93a682e5685ae16e73ef202221d
github <github>
2024-07-21 12:26:33 +0900

    プロフイメージ修正

--- sidebar.php
+++ sidebar.php
@@ -1,16 +1,13 @@
 <aside id="sidebar">
-<section class="archive">
-  <?php get_search_form(); ?>
-  </section>
   <section class="author">
     <img src="<?php echo esc_url(get_theme_file_uri('img/auther.svg')); ?>" alt="作者プロフィール">
     <h3 class="side-title">作者</h3>
     <p class="profile">
       歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者の<a href="https://github.com/dev-milk/sugar-design-wp">オリジナルテーマ</a>を使っています。現在、記事追加中です。</p>
   </section>
-  <!-- <section class="archive">
+  <section class="archive">
   <?php get_search_form(); ?>
-  </section> -->
+  </section>
  
   <section class="archive">
     <?php dynamic_sidebar('sidebar'); ?>
