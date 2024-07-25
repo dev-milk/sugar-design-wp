d12dda0a07e93dbb606da9a0b25d8f8d912a9f98
github <github>
2024-07-25 22:09:38 +0900

    見出し、サイドバー、フッターロゴ修正

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
 <aside id="sidebar">
   <section class="author">
     <img src="<?php echo esc_url(get_theme_file_uri('img/auther.svg')); ?>" alt="作者プロフィール">
-    <h3 class="side-title">作者</h3>
+    <h3 class="side-title">Auther</h3>
     <p class="profile">
-      歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者の<a href="https://github.com/dev-milk/sugar-design-wp">オリジナルテーマ</a>を使っています。現在、記事追加中です。</p>
+      歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者の<a href="https://github.com/dev-milk/sugar-design-wp">オリジナルテーマ</a>を使っています。</p>
   </section>
   <section class="archive">
   <?php get_search_form(); ?>
