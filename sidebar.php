c79f35710046e1d5de1595ac066ad2040e058157
github <github>
2024-07-28 00:28:04 +0900

    プロフ修正

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
     <img src="<?php echo esc_url(get_theme_file_uri('img/auther.svg')); ?>" alt="作者プロフィール">
     <h3 class="side-title">Auther</h3>
     <p class="profile">
-      歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者のオリジナルテーマ（ポートフォリオにて紹介）を採用しています</p>
+      歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者のオリジナルテーマ（ポートフォリオにて紹介）を使用しています</p>
   </section>
   <section class="archive">
   <?php get_search_form(); ?>
