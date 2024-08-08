3ab8a6b5a9846692593156437d0424d3d8c476d1
github <github>
2024-08-09 00:49:27 +0900

    footerにSNSボタン追加

--- footer.php
+++ footer.php
@@ -13,9 +13,26 @@
         <p>
         歴史が好きな有閑マダムを目指す普通の会社員です。<br>こちらのブログは作者のオリジナルテーマを採用しています。
         </p>
+        <ul>
+          <li >
+            <a href="https://www.pinterest.jp/devsugar_biz/">
+            <img src="<?php echo get_theme_file_uri( 'img/pinterest-btn.png' ); ?>" alt="ピンタレスト">
+            </a>
+          </li>
+          <li >
+            <a href="https://www.instagram.com/devsugar_biz/">
+            <img src="<?php echo get_theme_file_uri( 'img/insta-btn.png' ); ?>" alt="インスタ">
+            </a> 
+          </li>
+          <li >
+            <a href="https://www.youtube.com/@devmilksugar4645">
+            <img src="<?php echo get_theme_file_uri( 'img/youtube-btn.png' ); ?>" alt="Youtube">
+            </a>
+          </li>
+        </ul>
       </section>
     </div>
-    <div class="copyright">© sugar.geo.jp</a></div>  
+    <div class="copyright">© devsugar.biz</a></div>
   </footer></body>
 </html>
 <?php wp_footer(); ?>
