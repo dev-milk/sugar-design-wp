1c94d766d124b85ca54e4bcb585024ee27856b4f
github <github>
2024-08-09 22:03:20 +0900

    フッターにSNSボタン

--- footer.php
+++ footer.php
@@ -1,41 +1,40 @@
 <div id="re-top">
-	<a href="#top" class="gotop">TOP</a>
+  <a href="#top" class="gotop">TOP</a>
 </div>
 <footer id="footer">
-    <div class="content wrapper">
-      <section class="item">
-        <a href="index.php">
-          <img class="footer-logo" src="<?php echo get_theme_file_uri( 'img/site-logo.svg' ); ?>" alt="サイトロゴ" />
-        </a>      
-      </section>
-      <section class="item">
-        <h3 class="footer-title">About</h3>
-        <p>
+  <div class="content wrapper">
+    <section class="item">
+      <a href="index.php">
+        <img class="footer-logo" src="<?php echo get_theme_file_uri('img/site-logo.svg'); ?>" alt="サイトロゴ" />
+      </a>
+    </section>
+    <section class="item">
+      <h3 class="footer-title">About</h3>
+      <p>
         歴史が好きな有閑マダムを目指す普通の会社員です。<br>こちらのブログは作者のオリジナルテーマを採用しています。
-        </p>
-        <ul>
-          <li >
-            <a href="https://www.pinterest.jp/devsugar_biz/">
-            <img src="<?php echo get_theme_file_uri( 'img/pinterest-btn.png' ); ?>" alt="ピンタレスト">
-            </a>
-          </li>
-          <li >
-            <a href="https://www.instagram.com/devsugar_biz/">
-            <img src="<?php echo get_theme_file_uri( 'img/insta-btn.png' ); ?>" alt="インスタ">
-            </a> 
-          </li>
-          <li >
-            <a href="https://www.youtube.com/@devmilksugar4645">
-            <img src="<?php echo get_theme_file_uri( 'img/youtube-btn.png' ); ?>" alt="Youtube">
-            </a>
-          </li>
-        </ul>
-      </section>
-    </div>
-    <div class="copyright">© devsugar.biz</a></div>
-  </footer></body>
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.pinterest.jp/devsugar_biz/">
+            <img src="<?php echo get_theme_file_uri('img/pinterest-btn.png'); ?>" alt="ピンタレスト">
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/devsugar_biz/">
+            <img src="<?php echo get_theme_file_uri('img/insta-btn.png'); ?>" alt="インスタ">
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/@devmilksugar4645">
+            <img src="<?php echo get_theme_file_uri('img/youtube-btn.png'); ?>" alt="Youtube">
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
+  <div class="copyright">© devsugar.biz</a></div>
+</footer>
+</body>
+
 </html>
 <?php wp_footer(); ?>
-
-
-
