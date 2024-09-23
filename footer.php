eeefdd5a431bd871c99728830e8b6822318a27a7
github <github>
2024-09-23 22:46:08 +0900

    フッターにメニュー追加、サイドバーにSNSボタン追加

--- footer.php
+++ footer.php
@@ -9,27 +9,17 @@
       </a>
     </section>
     <section class="item">
+      <h3 class="footer-title">Contents</h3>
+      <a href="<?php echo esc_url(home_url()); ?>"><p>Articles-歴史記事</p></a>
+      <a href="<?php echo esc_url(home_url('/archive/')); ?>"><p>Archive-建築美術の記事</p></a>
+      <a href="<?php echo esc_url(home_url('/chronology/')); ?>"><p>Chronology-歴史年表</p></a>
+      <a href="<?php echo esc_url(home_url('/contact/')); ?>"><p>Contact-お問い合わせ</p></a>
+    </section>
+    <section class="item">
       <h3 class="footer-title">About</h3>
       <p>
         歴史が好きな有閑マダムを目指す普通の会社員です。<br>こちらのブログは作者のオリジナルテーマを採用しています。
       </p>
-      <ul>
-        <li>
-          <a href="https://www.pinterest.jp/devsugar_biz/">
-            <img src="<?php echo get_theme_file_uri('img/pinterest-btn.png'); ?>" alt="ピンタレスト">
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/devsugar_biz/">
-            <img src="<?php echo get_theme_file_uri('img/insta-btn.png'); ?>" alt="インスタ">
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/@devmilksugar4645">
-            <img src="<?php echo get_theme_file_uri('img/youtube-btn.png'); ?>" alt="Youtube">
-          </a>
-        </li>
-      </ul>
     </section>
   </div>
   <div class="copyright">© devsugar.biz</a></div>
--- sidebar.php
+++ sidebar.php
@@ -4,11 +4,23 @@
     <h3 class="side-title">Auther</h3>
     <p class="profile">
       歴史が好きな有閑マダムを目指す普通の会社員です。歴史記事を書きながらWEBデザイン・コーディングもやっています。こちらのブログは作者のオリジナルテーマを使用しています。</p>
+      <p>
+      <a href="https://www.pinterest.jp/devsugar_biz/">
+      <img src="<?php echo get_theme_file_uri('img/pinterest-btn.png'); ?>" alt="ピンタレスト">
+     </a>
+     <a href="https://www.instagram.com/devsugar_biz/">
+      <img src="<?php echo get_theme_file_uri('img/insta-btn.png'); ?>" alt="インスタ">
+    </a>
+    <a href="https://www.youtube.com/@devmilksugar4645">
+      <img src="<?php echo get_theme_file_uri('img/youtube-btn.png'); ?>" alt="Youtube">
+     </a>
+      </p>
   </section>
+
   <section class="archive">
   <?php get_search_form(); ?>
   </section>
- 
+
   <section class="archive">
     <?php dynamic_sidebar('sidebar'); ?>
   </section>
