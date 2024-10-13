edc42424e6068d889aeca2804cce9b8ce4d8fa65
github <github>
2024-10-13 21:23:57 +0900

    年表固定ページテンプレ作成

---
+++ page-chronology.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * Template Name: 年表テンプレート
+ */
+?>
+
+
+<?php get_header(); ?>
+
+<div id="container" class="wrapper">
+  <main>
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+        <article>
+          <h1 class="article-title"><?php the_title(); ?></h1>
+          <ul class="category-list">
+            <li><a href="<?php echo esc_url(home_url('/chronology/')); ?>">BC9世紀～1200年年代</a></li>
+            <li><a href="#">1300年代～</a></li>
+          </ul>
+          <h2 class="wp-block-heading">BC9世紀～1200年代</h2>
+<div class="cp_timeline">
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">BC900</p>
+<p class="flag">BC9世紀</p>
+</div>
+<div class="desc">
+<p>ケルト人が集落ごとに集まって生活</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">BC52</p>
+<p class="flag">BC52年</p>
+</div>
+<div class="desc">
+<p>カエサル，ガリア全土を征服</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">375</p>
+<p class="flag">375年</p>
+</div>
+<div class="desc">
+<p>ゲルマン民族がフン族に圧され、欧州大陸に移住を開始</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">476</p>
+<p class="flag">476年</p>
+</div>
+<div class="desc">
+<p>ゲルマン民族の傭兵隊長オドアケルにより、西ローマ帝国が崩壊</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">481</p>
+<p class="flag">481年</p>
+</div>
+<div class="desc">
+<p>クローヴィスによりメロヴィング朝フランク王国が成立</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">756</p>
+<p class="flag">756年</p>
+</div>
+<div class="desc">
+<p>ピピン３世がカロリング朝を始め、征服した領土の一部を教皇に寄進</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">800</p>
+<p class="flag">800年</p>
+</div>
+<div class="desc">
+<p>カール大帝が西ローマ皇帝として戴冠</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">843</p>
+<p class="flag">843年</p>
+</div>
+<div class="desc">
+<p>ヴェルダン条約により中フランク王国が東フランクと西フランクに統合。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">870</p>
+<p class="flag">870年</p>
+</div>
+<div class="desc">
+<p>メルセン条約により西フランク王国・東フランク王国・中部フランク王国が成立。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">882</p>
+<p class="flag">882年</p>
+</div>
+<div class="desc">
+<p>キエフ公国の成立。ウクライナ地域を中心にキエフ大公が封建制度を始める。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">962</p>
+<p class="flag">962年</p>
+</div>
+<div class="desc">
+<p>オットー１世の戴冠。東フランク国王オットー１世がマジャール人を撃退し、ローマ帝国皇帝として戴冠。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">987</p>
+<p class="flag">987年</p>
+</div>
+<div class="desc">
+<p>ユーグ・カペーがフランス国王となる。国名が西フランクからフランスに変わる。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1066</p>
+<p class="flag">1066年</p>
+</div>
+<div class="desc">
+<p>ノルマン＝コンクエスト。ギョーム２世のイングランド征服。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1077</p>
+<p class="flag">1077年</p>
+</div>
+<div class="desc">
+<p>カノッサの屈辱。聖職叙任権をめぐり教皇グレゴリウス7世と神聖ローマ皇帝ハインリヒ4世が対立し、破門されるもカノッサ城にて赦される。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1095</p>
+<p class="flag">1095年</p>
+</div>
+<div class="desc">
+<p>第１回十字軍遠征。教皇がクレルモン宗教会議を招集し、連合軍をエルサレムに送る。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1137</p>
+<p class="flag">1137年</p>
+</div>
+<div class="desc">
+<p>ルイ７世とアリエノール・ダキテーヌの結婚により、広大なアキテーヌ公領がフランス領となる。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1145</p>
+<p class="flag">1145年</p>
+</div>
+<div class="desc">
+<p>第２回十字軍始まる。レコンキスタなどを除いて目立った戦果はなく失敗に終わる。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1154</p>
+<p class="flag">1154年</p>
+</div>
+<div class="desc">
+<p>イングランド国王ヘンリー２世即位。1152年にアリエノール・ダキテーヌと結婚。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1189</p>
+<p class="flag">1189年</p>
+</div>
+<div class="desc">
+<p>第３回十字軍始まる。サラディンの活躍と十字軍側の不協和音によりイスラム側が勝利する。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1204</p>
+<p class="flag">1204年</p>
+</div>
+<div class="desc">
+<p>第4回十字軍がコンスタンティノープルを占領する。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1215</p>
+<p class="flag">1215年</p>
+</div>
+<div class="desc">
+<p>イングランド議会が大憲章（マグナ＝カルタ）を制定する。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1220</p>
+<p class="flag">1220年</p>
+</div>
+<div class="desc">
+<p>フリードリヒ２世神聖ローマ皇帝即位。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1228</p>
+<p class="flag">1228年</p>
+</div>
+<div class="desc">
+<p>第５回十字軍派遣。フリードリヒ２世がエルサレムを回復。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1229</p>
+<p class="flag">1229年</p>
+</div>
+<div class="desc">
+<p>フランス王ルイ９世、南仏のアルビジョワ派を制圧。</p>
+</div>
+</div>
+<div class="timeline_item">
+<div class="time_date">
+<p class="time">1240</p>
+<p class="flag">1240年</p>
+</div>
+<div class="desc">
+<p>モンゴル軍がキエフ公国を滅ぼし、ロシアの「タタールのくびき」が始まる。</p>
+</div>
+</div>
+</div>
+
+<ul class="category-list">
+  <li><a href="#">1300年～</a></li>
+</ul>
+        
+        </article>
+
+    <?php endwhile;
+    endif; ?>
+
+    <!-- 前の記事　次の記事 -->
+    <div class="navigation">
+      <ul class="page-numbers">
+        <li class="navileft"><?php next_post_link('« %link', 'Nex', TRUE, ''); ?></li>
+        <li class="naviright"><?php previous_post_link('%link »', 'Pre', TRUE, ''); ?></li>
+      </ul>
+    </div>
+
+  </main>
+
+  <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
